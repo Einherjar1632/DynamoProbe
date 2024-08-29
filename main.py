ae359b9d34bbdaf62b1681b2312bf0e408cbcb7d
Einherjar1632 <>
2024-08-29 13:07:34 +0900

    fixed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # .envファイルから環境変数を読み込む
+load_dotenv()  # .envファイルから環境変数を読込
 
 if not all([os.getenv('AWS_ACCESS_KEY_ID'), os.getenv('AWS_SECRET_ACCESS_KEY'), os.getenv('AWS_DEFAULT_REGION')]):
     raise ValueError("AWS認証情報が正しく設定されていません。.envファイルを確認してください。")
